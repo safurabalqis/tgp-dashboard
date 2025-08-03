bc0e690471a51400f3b483aabf19ab405458d770
Safura <safurabalqiss@gmail.com>
2025-08-03 15:57:41 +0800

    Revert "error"
    This reverts commit 8b0cdf47c975c83baeeba29fe38decddc77ac311.

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from config import Config
 from app.models.models import db, Crash
 from app.routes.offense import offense_bp
 from app.routes.impact import impact_bp
-from app.routes.environment import environment_bp
-
+import json
 
 # Create Flask app with correct template and static folders
 app = Flask(__name__, 
@@ -17,7 +16,6 @@ db.init_app(app)
 
 app.register_blueprint(offense_bp)
 app.register_blueprint(impact_bp)
-app.register_blueprint(environment_bp)
 
 
 # Sample Olympics data
@@ -52,10 +50,10 @@ def landing():
     return render_template('landing.html')
 
 
-@app.route('/environment')
-def environment():
-    """Environmental Analysis Page"""
-    return render_template('environment.html', data=SAMPLE_DATA)
+@app.route('/sports')
+def sports():
+    """Sports analysis page"""
+    return render_template('sports.html', data=SAMPLE_DATA)
 
 @app.route('/api/chart-data')
 def chart_data():
